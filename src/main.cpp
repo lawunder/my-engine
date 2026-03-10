151144db478f517df4b619d2f6dcc918280b4b07
Lawunder <zwunder824@gmail.com>
2026-03-09 21:03:01 -0400

    Rendered Ball, collisions work, scoring resets ball.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,18 @@
 #include <SDL3/SDL_main.h>
 #include <iostream>
 
+
+
+struct Paddle {
+    float x, y, w, h, speed;
+};
+
+struct Ball {
+    float x, y, w, h, x_velo, y_velo, speed;
+};
+
+
+
 int main(int argc, char* argv[])
 {
     // Initialize SDL
@@ -11,14 +23,14 @@ int main(int argc, char* argv[])
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("My Engine", 1280, 720, SDL_WINDOW_RESIZABLE);
+    int window_width = 1280;
+    int window_height = 720;
+    SDL_Window* window = SDL_CreateWindow("My Engine", window_width, window_height, SDL_WINDOW_RESIZABLE);
     if (!window){
         std::cerr << "Failed to create window: " << SDL_GetError() << "\n";
         SDL_Quit();
         return -1;
     }
-
-    // std::cout << "Window created successfully, creating renderer\n";
     
     // Create Renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
@@ -29,79 +41,162 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    // std::cout << "Renderer created successfully, entering event loop\n";
-
     bool running = true;
     SDL_Event event; // queue of events (inputs)
-    float rectangle_x = 0;
-    float rectangle_y = 0;
-    float rectangle_width = 100;
-    float rectangle_height = 200;
-    const float speed = 300.0f; // pixels per second
     int current_time = SDL_GetTicks();
     int previous_time = SDL_GetTicks();
+
+
+    int paddle_width = 25;
+    int paddle_height = 300;
+    float paddle_speed = 300;
+    Paddle left_paddle = {0, ((window_height/2) - (paddle_height/2)), paddle_width, paddle_height, paddle_speed};
+    Paddle right_paddle = {window_width - paddle_width, ((window_height/2) - (paddle_height/2)), paddle_width, paddle_height, paddle_speed};
+
+    int ball_size = 20;
+    srand(SDL_GetTicks()); // set random seed
+    float ball_x_velo;
+    if (SDL_GetTicks() % 2 == 0){
+        ball_x_velo = 1;
+    }
+    else{
+        ball_x_velo = -1;
+    }
+    float ball_y_velo = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+    float ball_speed = 1000;
+    Ball ball = {window_width/2, window_height/2, ball_size, ball_size, ball_x_velo, ball_y_velo, ball_speed};
+
+    bool score = false;
 
     while (running){
         // Process queue of events until no more
         while (SDL_PollEvent(&event)){
-            if (event.type == SDL_EVENT_QUIT)
+            if (event.type == SDL_EVENT_QUIT){
                 running = false;
-
-            if (event.type == SDL_EVENT_KEY_DOWN)
-                if (event.key.key == SDLK_ESCAPE)
+            }
+            if (event.type == SDL_EVENT_KEY_DOWN){
+                if (event.key.key == SDLK_ESCAPE){
                     running = false;
-            
-            // std::cout << event.type << "\n";
+                }
+            }
         }
 
+        // Handle Delta Time
         current_time = SDL_GetTicks();
         float delta = current_time - previous_time;  //delta is difference between last update in ms
         float delta_seconds = delta / 1000.0f; // divide delta  to get amount in 
-        float move = delta_seconds * speed;
         previous_time = current_time;
 
-        // Movement of rectangle
-        int w, h;
-        SDL_GetWindowSize(window, &w, &h);
+        // Initialize window sizes and input
+        int window_width, window_height;
+        SDL_GetWindowSize(window, &window_width, &window_height);
         const bool* keystate = SDL_GetKeyboardState(NULL);
+
+        // left paddle movement
+        if (keystate[SDL_SCANCODE_W]){
+            left_paddle.y -= (delta_seconds * left_paddle.speed);
+            if (left_paddle.y <= 0)
+              left_paddle.y = 0;
+        }
+        
+        if (keystate[SDL_SCANCODE_S]){
+            left_paddle.y += (delta_seconds * left_paddle.speed);
+            if (left_paddle.y >= (window_height - left_paddle.h))
+              left_paddle.y = (window_height - left_paddle.h);
+        }
+
+        // right paddle movement
         if (keystate[SDL_SCANCODE_UP]){
-            rectangle_y -= move;
-            if (rectangle_y <= 0)
-              rectangle_y = 0;
+            right_paddle.y -= (delta_seconds * right_paddle.speed);
+            if (right_paddle.y <= 0)
+              right_paddle.y = 0;
         }
         if (keystate[SDL_SCANCODE_DOWN]){
-            rectangle_y += move;
-            if (rectangle_y >= (h - rectangle_height))
-              rectangle_y = (h - rectangle_height);
+            right_paddle.y += (delta_seconds * right_paddle.speed);
+            if (right_paddle.y >= (window_height - right_paddle.h))
+              right_paddle.y = (window_height - right_paddle.h);
         }
-        if (keystate[SDL_SCANCODE_LEFT]){
-            rectangle_x -= move;
-            if (rectangle_x <= 0)
-              rectangle_x = 0;
+
+
+        //ball movement and collisions
+        ball.x += (delta_seconds * ball.speed) * ball.x_velo;
+        ball.y += (delta_seconds * ball.speed) * ball.y_velo;
+        if (ball.y <= 0){
+            ball.y_velo *= -1;
         }
-        if (keystate[SDL_SCANCODE_RIGHT]){
-            rectangle_x += move;
-            if (rectangle_x >= (w - rectangle_width))
-              rectangle_x = (w - rectangle_width);
+        if (ball.y >= (window_height - ball.h)){
+            ball.y_velo *= -1;
         }
-        // Create Rectangle Object
-        SDL_FRect rect;
-        rect.x = rectangle_x;
-        rect.y = rectangle_y;
-        rect.w = rectangle_width;
-        rect.h = rectangle_height;
+
+
+        if (ball.x <= left_paddle.w){
+            if (ball.y >= left_paddle.y && ball.y <= left_paddle.h + left_paddle.y){
+                ball.x_velo *= -1;
+            }
+            if (ball.x <= 0){
+                score = true;
+            }
+        }
+
+        if (ball.x >= window_width - right_paddle.w - ball.w){
+            if (ball.y >= right_paddle.y && ball.y <= right_paddle.h + right_paddle.y){
+                ball.x_velo *= -1;
+            }
+            if (ball.x >= window_width){
+                score = true;
+            }
+        }
+
+        if (score){
+            ball.x = window_width/2;
+            ball.y = window_height/2;
+            if (SDL_GetTicks() % 2 == 0){
+                ball_x_velo = 1;
+            }
+            else{
+                ball_x_velo = -1;
+            }
+            ball.y_velo = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+            score = false;
+        }
+
+
+        // Create Paddle Objects
+        SDL_FRect left_paddle_rect;
+        left_paddle_rect.x = left_paddle.x;
+        left_paddle_rect.y = left_paddle.y;
+        left_paddle_rect.w = left_paddle.w;
+        left_paddle_rect.h = left_paddle.h;
+
+        SDL_FRect right_paddle_rect;
+        right_paddle_rect.x = right_paddle.x;
+        right_paddle_rect.y = right_paddle.y;
+        right_paddle_rect.w = right_paddle.w;
+        right_paddle_rect.h = right_paddle.h;
+
+        SDL_FRect ball_rect;
+        ball_rect.x = ball.x;
+        ball_rect.y = ball.y;
+        ball_rect.w = ball.w;
+        ball_rect.h = ball.h;
 
         // Handles drawing
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 1, 1, 1, 255);
         SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderFillRect(renderer, &rect);
-        SDL_RenderPresent(renderer);
 
+        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255);
+        SDL_RenderFillRect(renderer, &left_paddle_rect);
+
+        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255);
+        SDL_RenderFillRect(renderer, &right_paddle_rect);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        SDL_RenderFillRect(renderer, &ball_rect);
+
+        SDL_RenderPresent(renderer);
 
         // SDL_Delay(32); // roughly 30fps
     }
-
 
     // Cleanup
     SDL_DestroyWindow(window);
