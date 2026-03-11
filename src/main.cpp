89ee483c3224e6cc65ed3ad57fe9340ac046b518
Lawunder <zwunder824@gmail.com>
2026-03-11 00:27:56 -0400

    Refactored for Input, Update, and Render functions. Added GameState

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,18 @@ struct Paddle {
 };
 
 struct Ball {
-    float x, y, w, h, x_velo, y_velo, speed;
+    float x, y, w, h, x_velo, y_velo, default_speed, speed;
+};
+
+struct GameState {
+    Ball ball;
+    Paddle left_paddle;
+    Paddle right_paddle;
+    int left_score;
+    int right_score;
+    bool running;
+    bool start_game;
+    bool score;
 };
 
 
@@ -31,6 +42,161 @@ void RenderText(SDL_Renderer* renderer, TTF_Font* font, const char* text, float 
 }
 
 
+void HandleInput(GameState& state, SDL_Event& event, float delta_seconds, int window_height){ 
+    // Process queue of events until no more
+    while (SDL_PollEvent(&event)){
+        if (event.type == SDL_EVENT_QUIT){
+            state.running = false;
+        }
+        if (event.type == SDL_EVENT_KEY_DOWN){
+            if (event.key.key == SDLK_ESCAPE){
+                state.running = false;
+            }
+        }
+    }
+
+    // Start Game
+    const bool* keystate = SDL_GetKeyboardState(NULL);
+    if (keystate[SDL_SCANCODE_SPACE]){
+        state.start_game = true;
+    }
+
+    // left paddle movement
+    if (keystate[SDL_SCANCODE_W]){
+        state.left_paddle.y -= (delta_seconds * state.left_paddle.speed);
+        if (state.left_paddle.y <= 0)
+            state.left_paddle.y = 0;
+    }
+    if (keystate[SDL_SCANCODE_S]){
+        state.left_paddle.y += (delta_seconds * state.left_paddle.speed);
+        if (state.left_paddle.y >= (window_height - state.left_paddle.h))
+            state.left_paddle.y = (window_height - state.left_paddle.h);
+    }
+
+    // right paddle movement
+    if (keystate[SDL_SCANCODE_UP]){
+        state.right_paddle.y -= (delta_seconds * state.right_paddle.speed);
+        if (state.right_paddle.y <= 0)
+            state.right_paddle.y = 0;
+    }
+    if (keystate[SDL_SCANCODE_DOWN]){
+        state.right_paddle.y += (delta_seconds * state.right_paddle.speed);
+        if (state.right_paddle.y >= (window_height - state.right_paddle.h))
+            state.right_paddle.y = (window_height - state.right_paddle.h);
+    }
+}
+
+
+void Update(GameState& state, float delta_seconds, int window_width, int window_height){
+    if (state.start_game){
+        //ball movement and collisions
+        float xmove = (delta_seconds * state.ball.speed) * state.ball.x_velo;
+        float ymove = (delta_seconds * state.ball.speed) * state.ball.y_velo;
+        float move_norm = std::pow(state.ball.x_velo, 2) + std::pow(state.ball.y_velo, 2); // Normalize velo vector
+        state.ball.x += (xmove / move_norm);
+        state.ball.y += (ymove / move_norm);
+
+        // flip y_velo on floor/ceiling
+        if (state.ball.y <= 0){
+            state.ball.y_velo *= -1;
+        }
+        if (state.ball.y >= (window_height - state.ball.h)){
+            state.ball.y_velo *= -1;
+        }
+
+        // Ball enters left scoring zone
+        if (state.ball.x <= state.left_paddle.w){
+            if (state.ball.y >= state.left_paddle.y && state.ball.y <= state.left_paddle.h + state.left_paddle.y){
+                state.ball.x_velo *= -1;
+                state.ball.speed *= 1.15;
+            }
+            if (state.ball.x <= 0){
+                state.right_score += 1;
+                state.score = true;
+                state.ball.speed = state.ball.default_speed;
+            }
+        }
+
+        // Ball enters right scoring zone
+        if (state.ball.x >= window_width - state.right_paddle.w - state.ball.w){
+            if (state.ball.y >= state.right_paddle.y && state.ball.y <= state.right_paddle.h + state.right_paddle.y){
+                state.ball.x_velo *= -1;
+                state.ball.speed *= 1.25;
+            }
+            if (state.ball.x >= window_width){
+                state.left_score += 1;
+                state.score = true;
+                state.ball.speed = state.ball.default_speed;
+            }
+        }
+    }
+
+    // Reset Ball on score
+    if (state.score){
+        state.ball.x = window_width/2.0f;
+        state.ball.y = window_height/2.0f;
+        if (SDL_GetTicks() % 2 == 0){
+            state.ball.x_velo = 1;
+        }
+        else{
+            state.ball.x_velo = -1;
+        }
+        state.ball.y_velo = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
+        state.score = false;
+        state.start_game = false;
+    }
+}
+
+
+void Render(const GameState& state, SDL_Renderer* renderer, TTF_Font* font, int window_width, int window_height){
+        // Create Paddle Objects
+        SDL_FRect left_paddle_rect;
+        left_paddle_rect.x = state.left_paddle.x;
+        left_paddle_rect.y = state.left_paddle.y;
+        left_paddle_rect.w = state.left_paddle.w;
+        left_paddle_rect.h = state.left_paddle.h;
+
+        SDL_FRect right_paddle_rect;
+        right_paddle_rect.x = state.right_paddle.x;
+        right_paddle_rect.y = state.right_paddle.y;
+        right_paddle_rect.w = state.right_paddle.w;
+        right_paddle_rect.h = state.right_paddle.h;
+
+        SDL_FRect ball_rect;
+        ball_rect.x = state.ball.x;
+        ball_rect.y = state.ball.y;
+        ball_rect.w = state.ball.w;
+        ball_rect.h = state.ball.h;
+
+        // DRAW
+        SDL_SetRenderDrawColor(renderer, 1, 1, 1, 255); // background
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255); // paddle color
+        SDL_RenderFillRect(renderer, &left_paddle_rect);
+        SDL_RenderFillRect(renderer, &right_paddle_rect);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255); // ball color
+        SDL_RenderFillRect(renderer, &ball_rect);
+
+        // Render all text for scoring and win conditions
+        std::string left_score_str = std::to_string(state.left_score);
+        const char* left_score_text = left_score_str.c_str();
+        RenderText(renderer, font, left_score_text, 10, 10);
+        std::string right_score_str = std::to_string(state.right_score);
+        const char* right_score_text = right_score_str.c_str();
+        RenderText(renderer, font, right_score_text, window_width - 20, 10);
+        if (state.left_score == 5){
+            const char* left_win_text = "LEFT WINS!";
+            RenderText(renderer, font, left_win_text, window_width / 2.0f, window_height / 2.0f);
+        }
+        if (state.right_score == 5){
+            const char* right_win_text = "RIGHT WINS!";
+            RenderText(renderer, font, right_win_text, window_width / 2.0f, window_height / 2.0f);
+        }
+        // RENDER
+        SDL_RenderPresent(renderer);
+}
+
+
 int main(int argc, char* argv[])
 {
     std::cout << "Starting...\n"; 
@@ -42,8 +208,8 @@ int main(int argc, char* argv[])
     }
 
     // Create a window
-    float window_width = 1280;
-    float window_height = 720;
+    int window_width = 1280;
+    int window_height = 720;
     SDL_Window* window = SDL_CreateWindow("My Engine", window_width, window_height, 0);
     if (!window){
         std::cerr << "Failed to create window: " << SDL_GetError() << "\n";
@@ -82,21 +248,14 @@ int main(int argc, char* argv[])
 
     // Vars
     srand(SDL_GetTicks()); // set random seed
-    bool running = true;
     SDL_Event event; // queue of events (inputs)
     int current_time = SDL_GetTicks();
     int previous_time = SDL_GetTicks();
-    bool score = false;
-    int left_score = 0;
-    int right_score = 0;
-    bool start_game = false;
 
     // PADDLES init
     float paddle_width = 25;
     float paddle_height = 300;
     float paddle_speed = 400;
-    Paddle left_paddle = {0, ((window_height/2.0f) - (paddle_height/2.0f)), paddle_width, paddle_height, paddle_speed};
-    Paddle right_paddle = {window_width - paddle_width, ((window_height/2.0f) - (paddle_height/2.0f)), paddle_width, paddle_height, paddle_speed};
 
     // BALL init
     float ball_size = 20;
@@ -109,166 +268,32 @@ int main(int argc, char* argv[])
     }
     float ball_y_velo = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
     float ball_speed = 600;
-    Ball ball = {window_width/2.0f, window_height/2.0f, ball_size, ball_size, ball_x_velo, ball_y_velo, ball_speed};
+
+    // GAMESTATE init
+    GameState state;
+    state.ball = {window_width/2.0f, window_height/2.0f, ball_size, ball_size, ball_x_velo, ball_y_velo, ball_speed, ball_speed};
+    state.left_paddle = {0, ((window_height/2.0f) - (paddle_height/2.0f)), paddle_width, paddle_height, paddle_speed};
+    state.right_paddle = {window_width - paddle_width, ((window_height/2.0f) - (paddle_height/2.0f)), paddle_width, paddle_height, paddle_speed};
+    state.left_score = 0;
+    state.right_score = 0;
+    state.running = true;
+    state.start_game = false;
+    state.score = false;
+
 
     // LOOP
-    while (running){
-        // Process queue of events until no more
-        while (SDL_PollEvent(&event)){
-            if (event.type == SDL_EVENT_QUIT){
-                running = false;
-            }
-            if (event.type == SDL_EVENT_KEY_DOWN){
-                if (event.key.key == SDLK_ESCAPE){
-                    running = false;
-                }
-            }
-        }
-
-
-        // Handle Delta Time
+    while (state.running){
+        // Delta Time
         current_time = SDL_GetTicks();
         float delta = current_time - previous_time;  //delta is difference between last update in ms
         float delta_seconds = delta / 1000.0f; // divide delta  to get amount in 
         previous_time = current_time;
 
-        // Initialize window sizes
-        int window_width, window_height;
         SDL_GetWindowSize(window, &window_width, &window_height);
-
-        // Input
-        const bool* keystate = SDL_GetKeyboardState(NULL);
-        if (keystate[SDL_SCANCODE_SPACE]){
-            start_game = true;
-        }
-
-        // left paddle movement
-        if (keystate[SDL_SCANCODE_W]){
-            left_paddle.y -= (delta_seconds * left_paddle.speed);
-            if (left_paddle.y <= 0)
-              left_paddle.y = 0;
-        }
-        if (keystate[SDL_SCANCODE_S]){
-            left_paddle.y += (delta_seconds * left_paddle.speed);
-            if (left_paddle.y >= (window_height - left_paddle.h))
-              left_paddle.y = (window_height - left_paddle.h);
-        }
-
-        // right paddle movement
-        if (keystate[SDL_SCANCODE_UP]){
-            right_paddle.y -= (delta_seconds * right_paddle.speed);
-            if (right_paddle.y <= 0)
-              right_paddle.y = 0;
-        }
-        if (keystate[SDL_SCANCODE_DOWN]){
-            right_paddle.y += (delta_seconds * right_paddle.speed);
-            if (right_paddle.y >= (window_height - right_paddle.h))
-              right_paddle.y = (window_height - right_paddle.h);
-        }
-
-        if (start_game){
-            //ball movement and collisions
-            float xmove = (delta_seconds * ball.speed) * ball.x_velo;
-            float ymove = (delta_seconds * ball.speed) * ball.y_velo;
-            float move_norm = std::pow(ball.x_velo, 2) + std::pow(ball.y_velo, 2); // Normalize velo vector
-            ball.x += (xmove / move_norm);
-            ball.y += (ymove / move_norm);
-
-            // flip y_velo on floor/ceiling
-            if (ball.y <= 0){
-                ball.y_velo *= -1;
-            }
-            if (ball.y >= (window_height - ball.h)){
-                ball.y_velo *= -1;
-            }
-
-            // Ball enters left scoring zone
-            if (ball.x <= left_paddle.w){
-                if (ball.y >= left_paddle.y && ball.y <= left_paddle.h + left_paddle.y){
-                    ball.x_velo *= -1;
-                    ball.speed *= 1.15;
-                }
-                if (ball.x <= 0){
-                    right_score += 1;
-                    score = true;
-                    ball.speed = ball_speed;
-                }
-            }
-
-            // Ball enters right scoring zone
-            if (ball.x >= window_width - right_paddle.w - ball.w){
-                if (ball.y >= right_paddle.y && ball.y <= right_paddle.h + right_paddle.y){
-                    ball.x_velo *= -1;
-                    ball.speed *= 1.25;
-                }
-                if (ball.x >= window_width){
-                    left_score += 1;
-                    score = true;
-                    ball.speed = ball_speed;
-                }
-            }
-        }
-
-        // Reset Ball on score
-        if (score){
-            ball.x = window_width/2.0f;
-            ball.y = window_height/2.0f;
-            if (SDL_GetTicks() % 2 == 0){
-                ball.x_velo = 1;
-            }
-            else{
-                ball.x_velo = -1;
-            }
-            ball.y_velo = ((float)rand() / RAND_MAX) * 2.0f - 1.0f;
-            score = false;
-            start_game = false;
-        }
-        
-
-        // Create Paddle Objects
-        SDL_FRect left_paddle_rect;
-        left_paddle_rect.x = left_paddle.x;
-        left_paddle_rect.y = left_paddle.y;
-        left_paddle_rect.w = left_paddle.w;
-        left_paddle_rect.h = left_paddle.h;
-
-        SDL_FRect right_paddle_rect;
-        right_paddle_rect.x = right_paddle.x;
-        right_paddle_rect.y = right_paddle.y;
-        right_paddle_rect.w = right_paddle.w;
-        right_paddle_rect.h = right_paddle.h;
-
-        SDL_FRect ball_rect;
-        ball_rect.x = ball.x;
-        ball_rect.y = ball.y;
-        ball_rect.w = ball.w;
-        ball_rect.h = ball.h;
-
-        // DRAW
-        SDL_SetRenderDrawColor(renderer, 1, 1, 1, 255); // background
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 123, 123, 123, 255); // paddle color
-        SDL_RenderFillRect(renderer, &left_paddle_rect);
-        SDL_RenderFillRect(renderer, &right_paddle_rect);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255); // ball color
-        SDL_RenderFillRect(renderer, &ball_rect);
-
-        // Render all text for scoring and win conditions
-        const char* left_score_text = std::to_string(left_score).c_str();
-        RenderText(renderer, font, left_score_text, 10, 10);
-        const char* right_score_text = std::to_string(right_score).c_str();
-        RenderText(renderer, font, right_score_text, window_width - 20, 10);
-        if (left_score == 5){
-            const char* left_win_text = "LEFT WINS!";
-            RenderText(renderer, font, left_win_text, window_width / 2.0f, window_height / 2.0f);
-        }
-        if (right_score == 5){
-            const char* right_win_text = "RIGHT WINS!";
-            RenderText(renderer, font, right_win_text, window_width / 2.0f, window_height / 2.0f);
-        }
-        // RENDER
-        SDL_RenderPresent(renderer);
-        
+        HandleInput(state, event, delta_seconds, window_height);
+        Update(state, delta_seconds, window_width, window_height);
+        Render(state, renderer, font, window_width, window_height);
+    
         // SDL_Delay(32); // delay by 32ms, roughly 30fps
     }
 
